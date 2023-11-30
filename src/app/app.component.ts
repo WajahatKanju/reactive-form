ab6065d484cbe99f2ce308c59a775c2cd12e2101
WajahatKanju <wajahat5ahmad@gmail.com>
2023-11-30 11:28:35 +0500

    added visual basic controls, and validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {Form01Component} from "./form01/form01.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule , RouterOutlet , Form01Component],
-  templateUrl: './app.component.html',
+  selector: 'app-root' ,
+  standalone: true ,
+  imports: [ReactiveFormsModule , CommonModule , RouterOutlet , Form01Component] ,
+  templateUrl: './app.component.html' ,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- src/app/form01/form01.component.ts
+++ src/app/form01/form01.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component , OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormControl , FormGroup , ReactiveFormsModule , Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-form01',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './form01.component.html',
+  selector: 'app-form01' ,
+  standalone: true ,
+  imports: [CommonModule , ReactiveFormsModule] ,
+  templateUrl: './form01.component.html' ,
   styleUrl: './form01.component.scss'
 })
-export class Form01Component {
+export class Form01Component implements OnInit {
+  reactiveForm: FormGroup = new FormGroup({});
 
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      firstName: new FormControl(null , [Validators.required, Validators.minLength(3)],) ,
+      lastName: new FormControl('Doe' , Validators.required ,) ,
+      email: new FormControl('jondoe@example.com' , [Validators.required , Validators.email ,]) ,
+      username: new FormControl('jon.doe' , Validators.required ,) ,
+      dob: new FormControl('2000-01-01' , Validators.required ,) ,
+      gender: new FormControl('male' , Validators.required ,) ,
+      street: new FormControl('123 @ Street' , Validators.required ,) ,
+      country: new FormControl('Pakistan' , Validators.required ,) ,
+      city: new FormControl('Islamabad' , Validators.required ,) ,
+      region: new FormControl('Punjab' , Validators.required ,) ,
+      postal: new FormControl('123456' , Validators.required ,) ,
+    })
+  }
+
+  FormSubmitted() {
+    console.log(this.reactiveForm)
+  }
 }
